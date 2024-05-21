9588beeb41cf334f59331968b70f2e64144e664f
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2024-05-21 16:13:44 +0200

    fix: delete test code

--- packages/angular-workspace/projects/component-library/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of component-library
- */
-
-export * from './lib/components.module';
-export { DIRECTIVES } from './lib/stencil-generated';
-export * from './lib/stencil-generated/components';
